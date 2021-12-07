ce7ab64116373bab3c62ccf280dafca62db24aeb
vladhaidukkk <the.vladhaiduk@gmail.com>
2021-12-07 22:37:15 +0200

    #1: create first version of React Carousel Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Carousel, { CarouselItem } from './carousel/carousel';
+import { carouselItemsContent } from './data/carouselItemsContent';
+import './app.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Carousel width='80%' itemsOnSlide={1}>
+        {carouselItemsContent.map((carouselItem, index) => {
+          return <CarouselItem key={index}>{carouselItem}</CarouselItem>;
+        })}
+      </Carousel>
     </div>
   );
-}
+};
 
 export default App;
